1ab336295c3ce55458db277e37087cd4d66e6393
Joseph Duchesne <josephgeek@gmail.com>
2019-04-14 15:19:18 -0400

    commented configuration

--- config/config.go
+++ config/config.go
@@ -6,6 +6,8 @@ import (
 	"log"
 )
 
+// This data structure mirrors the config.yaml file
+// See config.yaml.dist for a field by field description
 type Config struct {
 	Source     string            `yaml:"source"`
 	Extensions []string          `yaml:"extensions"`
@@ -14,6 +16,7 @@ type Config struct {
 	Aliases    map[string]string `yaml:"aliases"`
 }
 
+// Load config.yaml into the Config data structure
 func (c *Config) GetConfig() *Config {
 	yamlFile, err := ioutil.ReadFile("config.yaml")
 	if err != nil {
