e028c9b1c47e7dd2423cf31a2b5a923f2b4d34aa
Joseph Duchesne <josephgeek@gmail.com>
2019-04-14 16:41:48 -0400

    Added unit test to config loading

---
+++ config/config_test.go
@@ -0,0 +1,28 @@
+package config_test
+
+import (
+    "github.com/josephduchesne/episode_organizer/config"
+	"testing"
+    "reflect"
+    "fmt"
+)
+
+// TestConfig is the GetConfig Config loading unit test
+func TestGetConfig(t *testing.T) {
+    var input config.Config
+    input.GetConfig("testdata/config.yaml")
+
+    expected := config.Config{
+        Source: "/some/source",
+        Extensions: []string{"A", "B"},
+        Aliases: map[string]string{"Foo": "Bar", "Baz":"Bash"},
+        Dest: "/some/dest",
+        MinSize: 123,
+    }
+    if reflect.DeepEqual(input,expected) {
+        fmt.Println("Configuration loaded as expected")
+    } else {
+        fmt.Printf("Config mismatch:\nExpected: %v\nRead: %v", expected, input)
+        t.Fail()
+    }
+}
