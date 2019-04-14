629ef1ad4634b818877f369d71d5eac201faa66f
Joseph Duchesne <josephgeek@gmail.com>
2019-04-14 15:16:22 -0400

    ran gofmt -s

--- config/config.go
+++ config/config.go
@@ -1,30 +1,28 @@
 package config
 
 import (
-    "gopkg.in/yaml.v2"
-    "io/ioutil"
-    "log"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
 )
 
 type Config struct {
-    Source string `yaml:"source"`
-    Extensions []string `yaml:"extensions"`
-    Dest string `yaml:"dest"`
-    MinSize int64 `yaml:"min_size"`
-    Aliases map[string]string `yaml:"aliases"`
+	Source     string            `yaml:"source"`
+	Extensions []string          `yaml:"extensions"`
+	Dest       string            `yaml:"dest"`
+	MinSize    int64             `yaml:"min_size"`
+	Aliases    map[string]string `yaml:"aliases"`
 }
 
-
-
 func (c *Config) GetConfig() *Config {
-    yamlFile, err := ioutil.ReadFile("config.yaml")
-    if err != nil {
-        log.Printf("yamlFile.Get err   #%v ", err)
-    }
-    err = yaml.Unmarshal(yamlFile, c)
-    if err != nil {
-        log.Fatalf("Unmarshal: %v", err)
-    }
+	yamlFile, err := ioutil.ReadFile("config.yaml")
+	if err != nil {
+		log.Printf("yamlFile.Get err   #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
 
-    return c
+	return c
 }
--- organize/organize.go
+++ organize/organize.go
@@ -1,93 +1,93 @@
 package organize
 
 import (
-    "os"
-    "log"
-    "strings"
-    "path/filepath"
-    "regexp"
-    "errors"
+	"errors"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
 )
 
 type Episode struct {
-    Path string
-    Filename string
-    Series string
-    Season string
+	Path     string
+	Filename string
+	Series   string
+	Season   string
 }
 
 // Move episode to the destination subfolder, if it exists
 func MoveEpisode(episode Episode, dest string) {
-    r := strings.NewReplacer("{Series}", episode.Series,
-                             "{Season}", episode.Season,
-                             "{Filename}", episode.Filename)
-    dest_path := r.Replace(dest)
-    dest_dir  := filepath.Dir(dest_path)
+	r := strings.NewReplacer("{Series}", episode.Series,
+		"{Season}", episode.Season,
+		"{Filename}", episode.Filename)
+	dest_path := r.Replace(dest)
+	dest_dir := filepath.Dir(dest_path)
 
-    if _, err := os.Stat(dest_dir); !os.IsNotExist(err) {
-        err := os.Rename(episode.Path, dest_path)
-        if err != nil {
-            log.Printf("Failed to move %s to %s: %v\n", episode.Path, dest_path, err)
-        } else {
-            log.Printf("Moved %s to %s\n", episode.Path, dest_path)
-        }
-    } else {
-        log.Printf("No destination found for %s\n", dest_dir)
-    }
+	if _, err := os.Stat(dest_dir); !os.IsNotExist(err) {
+		err := os.Rename(episode.Path, dest_path)
+		if err != nil {
+			log.Printf("Failed to move %s to %s: %v\n", episode.Path, dest_path, err)
+		} else {
+			log.Printf("Moved %s to %s\n", episode.Path, dest_path)
+		}
+	} else {
+		log.Printf("No destination found for %s\n", dest_dir)
+	}
 }
 
 func ParseEpisode(path string, aliases map[string]string) (Episode, error) {
-    var e Episode
-    var err error
-    e.Path = path
-    e.Filename = filepath.Base(path)
+	var e Episode
+	var err error
+	e.Path = path
+	e.Filename = filepath.Base(path)
 
-    // Pull out episode name and season (stripping off leading zeros)
-    r, err := regexp.Compile(`(.+)[sS]0*([\d]+)[eE][\d]+.+`)
-    if err != nil {
-        return e,err
-    }
-    res := r.FindStringSubmatch(e.Filename)
-    if len(res) != 3 {
-        return e,errors.New("Failed to find episode name and season")
-    }
+	// Pull out episode name and season (stripping off leading zeros)
+	r, err := regexp.Compile(`(.+)[sS]0*([\d]+)[eE][\d]+.+`)
+	if err != nil {
+		return e, err
+	}
+	res := r.FindStringSubmatch(e.Filename)
+	if len(res) != 3 {
+		return e, errors.New("Failed to find episode name and season")
+	}
 
-    // "." is space, and ToTitle makes each word upper-case-first
-    e.Series = strings.Title(strings.ToLower(strings.ReplaceAll(res[1], ".", " ")))
-    e.Season = res[2]
+	// "." is space, and ToTitle makes each word upper-case-first
+	e.Series = strings.TrimSpace(strings.Title(strings.ToLower(strings.ReplaceAll(res[1], ".", " "))))
+	e.Season = res[2]
 
-    // Handle episode alises
-    if val, ok := aliases[e.Series]; ok {
-        // log.Printf("Series alias: `%s` -> `%s`\n", e.Series, val)
-        e.Series = val
-    }
+	// Handle episode alises
+	if val, ok := aliases[e.Series]; ok {
+		// log.Printf("Series alias: `%s` -> `%s`\n", e.Series, val)
+		e.Series = val
+	}
 
-    return e, err
+	return e, err
 }
 
 // Find all video files that meet the min-size and extension criteria
 func GetVideoFiles(folder string, min_size int64, extensions []string) []string {
-    var video_files []string;
-    err := filepath.Walk(folder,
-        func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            return err
-        }
-        if (info.Size()>=min_size){
-            //fmt.Println(path, info.Size())
-            for _, extension := range extensions {
-                if strings.HasSuffix(path, extension) {
-                    // Record file path for later return
-                    video_files = append(video_files, path)
-                    return nil  // Done processing this file
-                }
-            }
-            log.Println("Unknown possible video format: %s", path)
-        }
-        return nil
-    })
-    if err != nil {
-        log.Println(err)
-    }
-    return video_files
+	var video_files []string
+	err := filepath.Walk(folder,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if info.Size() >= min_size {
+				//fmt.Println(path, info.Size())
+				for _, extension := range extensions {
+					if strings.HasSuffix(path, extension) {
+						// Record file path for later return
+						video_files = append(video_files, path)
+						return nil // Done processing this file
+					}
+				}
+				log.Println("Unknown possible video format: %s", path)
+			}
+			return nil
+		})
+	if err != nil {
+		log.Println(err)
+	}
+	return video_files
 }
